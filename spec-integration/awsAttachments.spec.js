0979deeeba0b1ca36c33d3579ef4a8db7462c41d
ElBabbler <gladikov.denis@gmail.com>
2019-03-12 12:39:13 +0200

    Adding addAttachment implementation for AWS S3 storage.

---
+++ spec-integration/awsAttachments.spec.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const chai = require('chai');
+const { Readable } = require('stream');
+
+const { expect } = chai;
+
+const awsAttachments = require('../lib/awsAttachments');
+
+describe('Aws attachment', function() {
+  this.timeout(50000);
+
+  before(async () => {
+    if (fs.existsSync('.env')) {
+      require('dotenv').config();
+    }
+  });
+
+  it('adding attachment', async () => {
+    const stream = fs.createReadStream('spec-integration/samples/test.csv');
+    const result = await awsAttachments.addAttachment({}, 'test.csv', stream, 1215);
+      expect(Object.keys(result.attachments)[0]).to.eql('test.csv');
+  });
+});
