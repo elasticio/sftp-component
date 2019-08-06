fc93dbbff2f0224a0c1bcae0b8e1da7748bf30e4
Shanthosh Pushparajah <spushpar@edu.uwaterloo.ca>
2019-08-06 17:16:17 +0200

    refactor unit tests

--- spec/verifyCredentials.spec.js
+++ spec/verifyCredentials.spec.js
@@ -1,74 +1,44 @@
-// describe("SFTP Verify Credentials", function () {
-//
-//     const verifyCredentials = require('../verifyCredentials.js');
-//     const sftp = require('../lib/sftp.js');
-//
-//     let verifyCallback;
-//
-//
-//     beforeEach(function () {
-//         verifyCallback = jasmine.createSpy('verifyCallback');
-//
-//         spyOn(sftp, 'close').andCallFake(function () {
-//         });
-//     });
-//
-//
-//     it('should verify successfully', function () {
-//
-//         spyOn(sftp, 'connect').andCallFake(function (cfg, callback) {
-//             callback(null, {});
-//         });
-//
-//
-//         runAndWait(
-//             function () {
-//                 verifyCredentials({}, verifyCallback);
-//             },
-//             function () {
-//                 return verifyCallback.calls.length > 0;
-//             },
-//             function () {
-//                 expect(sftp.connect).toHaveBeenCalledWith({}, jasmine.any(Function));
-//
-//                 expect(sftp.close).toHaveBeenCalled();
-//
-//                 expect(verifyCallback).toHaveBeenCalledWith(null, {verified: true});
-//             }
-//         );
-//     });
-//
-//     it('should not verify if connection failed', function () {
-//
-//         spyOn(sftp, 'connect').andCallFake(function (cfg, callback) {
-//             callback(new Error("Connection error"));
-//         });
-//
-//
-//         runAndWait(
-//             function () {
-//                 verifyCredentials({}, verifyCallback);
-//             },
-//             function () {
-//                 return verifyCallback.calls.length > 0;
-//             },
-//             function () {
-//                 expect(sftp.connect).toHaveBeenCalledWith({}, jasmine.any(Function));
-//
-//                 expect(sftp.close).not.toHaveBeenCalled();
-//
-//                 expect(verifyCallback).toHaveBeenCalledWith(jasmine.any(Error));
-//             }
-//         );
-//     });
-//
-//
-//     var runAndWait = function (runner, waiter, expector) {
-//
-//         runs(runner);
-//
-//         waitsFor(waiter, "Next must have been called", 500);
-//
-//         runs(expector);
-//     };
-// });
+const verifyCredentials = require('../verifyCredentials.js');
+const sftp = require('../lib/sftp.js');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+let verifyCallback = {};
+
+describe("SFTP Verify Credentials", function () {
+
+    afterEach(() => {
+        sinon.reset();
+    });
+
+
+    it('should verify successfully', async function () {
+
+         sinon.stub(sftp, 'connect').callsFake(function (cfg, callback) {
+            callback(null, {});
+        });
+
+         await verifyCredentials({}, verifyCallback);
+
+       //  expect(sftp.connect).toHaveBeenCalledWith({}, jasmine.any(Function));
+         expect(sftp.close).toHaveBeenCalled();
+         expect(verifyCallback).toHaveBeenCalledWith(null, {verified: true});
+
+    });
+
+    it('should not verify if connection failed', async function () {
+
+        sinon.stub(sftp, 'connect').callsFake(function (cfg, callback) {
+            callback(new Error("Connection error"));
+        });
+
+        await verifyCredentials({}, verifyCallback);
+
+        // expect(sftp.connect).toHaveBeenCalledWith({}, jasmine.any(Function));
+
+        expect(sftp.close).not.toHaveBeenCalled();
+
+        expect(verifyCallback).toHaveBeenCalledWith(jasmine.any(Error));
+
+    });
+});
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,24 +1,24 @@
-// const sftp = require('./lib/sftp.js');
-//
-// module.exports = verify;
-//
-// function verify(cfg, cb) {
-//
-//     console.log('Verifying the SFTP account');
-//
-//     sftp.connect(cfg, function (err, client) {
-//
-//         if (err) {
-//             return cb(err);
-//         }
-//
-//         sftp.close(client);
-//
-//         cb(null, {
-//             verified: true
-//         });
-//
-//         console.log('SFTP account verified successfully');
-//     });
-//
-// }
+const sftp = require('./lib/sftp.js');
+
+module.exports = verify;
+
+function verify(cfg, cb) {
+
+    console.log('Verifying the SFTP account');
+
+    sftp.connect(cfg, function (err, client) {
+
+        if (err) {
+            return cb(err);
+        }
+
+        sftp.close(client);
+
+        cb(null, {
+            verified: true
+        });
+
+        console.log('SFTP account verified successfully');
+    });
+
+}
