358f5bf44a8ae8facceebb0e135031b6f38828bc
David Zhou <david.zhou@elastic.io>
2019-09-20 10:24:08 +0200

    Delete console logs

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,8 +1,6 @@
 const sftp = require('./lib/sftp.js');
 
 function verify(cfg, cb) {
-  console.log('Verifying the SFTP account');
-
   sftp.connect(cfg, (err, client) => {
     if (err) {
       cb(err, { verified: false });
@@ -11,7 +9,6 @@ function verify(cfg, cb) {
 
     sftp.close(client);
     cb(null, { verified: true });
-    console.log('SFTP account verified successfully');
     return { verified: true };
   });
 }
