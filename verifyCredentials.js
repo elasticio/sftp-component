f0cb64816bfc1c8fd30dbaa22d729a479b19b8af
David Zhou <david.zhou@elastic.io>
2019-09-26 15:51:27 +0200

    Refactor verifyCredentials

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,16 +1,14 @@
-const sftp = require('./lib/sftp.js');
+const Sftp = require('./lib/Sftp');
 
-function verify(cfg, cb) {
-  sftp.connect(cfg, (err, client) => {
-    if (err) {
-      cb(err, { verified: false });
-      return { verified: false };
-    }
-
-    sftp.close(client);
-    cb(null, { verified: true });
-    return { verified: true };
-  });
-}
-
-module.exports = verify;
+module.exports = async function verify(cfg, cb) {
+  try {
+    const sftp = new Sftp(this.logger, cfg);
+    await sftp.connect();
+    await sftp.end();
+    this.logger.info('SFTP verified');
+    return cb(null, { verified: true });
+  } catch (err) {
+    this.logger.error(err);
+    return cb(err, { verified: false });
+  }
+};
