938f21ae4724cc7ea80f815a35bb64a9dde54608
Olha Virolainen <olga.shukel@gmail.com>
2019-12-17 15:11:38 +0200

    Support key-based auth

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -2,6 +2,12 @@ const Sftp = require('./lib/Sftp');
 
 module.exports = async function verify(cfg, cb) {
   try {
+    const { password, privateKey } = cfg;
+    if (password && privateKey) {
+      throw new Error('Both: password and private key fields are filled, use only one option');
+    } else if (!password && !privateKey) {
+      throw new Error('Both: password and private key fields are empty, use only one option');
+    }
     const sftp = new Sftp(this.logger, cfg);
     await sftp.connect();
     await sftp.end();
