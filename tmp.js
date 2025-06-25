b1c1b13684ef290036591203aef466e4be33d008
if0s <if0s.3d@gmail.com>
2025-06-25 16:17:32 +0300

    up

--- tmp.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-async function test() {
-  let result = sleep(1000);
-  console.log(result);
-}
-
-test()
